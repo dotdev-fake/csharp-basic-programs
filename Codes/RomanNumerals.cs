a6e3999a2ab7ed38924191aa8c019b21d4462f96
.dev <104110725+dotdev-fake@users.noreply.github.com>
2025-01-06 19:47:56 +0800

    Update RomanNumerals.cs

--- Codes/RomanNumerals.cs
+++ Codes/RomanNumerals.cs
@@ -1,6 +1,7 @@
 public class Solution {
     public int RomanToInt(string s) {
-        
+
+        // Define roman numerals, Note: They go in ascending order, which is useful for solving
         Dictionary<char, int> symbolTable = new Dictionary<char, int>();
         symbolTable['I'] = 1;
         symbolTable['V'] = 5;
@@ -11,17 +12,17 @@ public class Solution {
         symbolTable['M'] = 1000;
 
         int output = 0;
-        for (int i=0;i<s.Length - 1;i++){
+        for (int i=0;i<s.Length - 1;i++){ // Only iterate until second to the last value
             if (symbolTable.ContainsKey(s[i]) && symbolTable.ContainsKey(s[i+1])){
                 int val1 = symbolTable[s[i]];
                 int val2 = symbolTable[s[i+1]];
 
-                if (val1<val2) output -= val1;
-                else output += val1;
+                if (val1<val2) output -= val1; // If current roman numeral is lesser than the next numeral, subtract the current upcoming numeral value.
+                else output += val1; // Normal addition if its not lesser than
             }
         }
 
-        output += symbolTable[s[s.Length-1]];
+        output += symbolTable[s[s.Length-1]]; // Add the last value to complete the loop and roman numeral
         return output;
     }
 }
